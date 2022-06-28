15b2abe14b2ab86bb294a2ecb94a3006e648d490
Alexander Winters <wintersalex@icloud.com>
2022-06-28 16:25:24 -0700

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    const word = text.split("");
+    const result = [];
 
+    for (let i = 0; i < text.length; i++) {
+        result[i] = word.pop();
+    }
+
+    return result.join("");
 };
 
 // Do not edit below this line
