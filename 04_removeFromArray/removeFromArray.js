87d943624d67988093c81a25e623fe9233af86e0
Alexander Winters <wintersalex@icloud.com>
2022-06-28 17:11:11 -0700

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numArray, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < numArray.length; j++) {
+            if (numArray[j] === args[i]) {
+                numArray.splice(j, 1);
+            }
+        }
+    }
+    return numArray;
 };
 
 // Do not edit below this line
