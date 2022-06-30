8a17dff0c40ae96eef5590c03299b1fbb833141a
Alexander Winters <wintersalex@icloud.com>
2022-06-30 12:06:22 -0700

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  let sum = 0;
+  for (let i = 0; i  < sumArray.length; i++) {
+    sum += sumArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+  let sum = 1;
+  for (let i = 0; i < multiplyArray.length; i++) {
+    sum *= multiplyArray[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	let sum = 1;
+  for (let i = 0; i < y; i++) {
+    sum *= x;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let sum = 1;
+  for (let i = x; i > 0; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
