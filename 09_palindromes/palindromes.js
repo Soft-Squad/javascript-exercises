984da4171003312f04b394276f3a46f2c360f7d3
Alexander Winters <wintersalex@icloud.com>
2022-06-30 12:42:01 -0700

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let processedString = text.toLowerCase().replace(/[^a-z]/g, "");
+    
+    return (processedString.split("").reverse().join("") == processedString);
 };
 
 // Do not edit below this line
