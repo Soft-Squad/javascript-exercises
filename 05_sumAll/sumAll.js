a33f16f9b907cb5ebabb825dd117bb63d53f89bc
Alexander Winters <wintersalex@icloud.com>
2022-06-30 11:07:55 -0700

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(startNum, finalNum) {
+    let sum = 0;
+    
+    if (startNum < 0 || finalNum < 0 || typeof startNum != "number" || typeof finalNum != "number") return "ERROR";
 
+    if (startNum > finalNum) {
+        let temp = startNum;
+        startNum = finalNum;
+        finalNum = temp;
+    }
+    for (let i = startNum; i <= finalNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
