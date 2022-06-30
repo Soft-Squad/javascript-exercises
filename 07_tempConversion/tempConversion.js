0c80f5bf8db1c483fe92a8f99f4d25f387ad5874
Alexander Winters <wintersalex@icloud.com>
2022-06-30 11:53:18 -0700

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5/9);
+  if (cTemp === 0) {
+    return 0;
+  }
+  return round(cTemp, 1);
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = cTemp * (9/5) + 32;
+  if (fTemp === 0) {
+    return 0;
+  }
+  return round(fTemp, 1);
 };
+
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
