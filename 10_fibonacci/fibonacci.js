1ef312099c7899ead34c59b44ed281841dd81e08
Alexander Winters <wintersalex@icloud.com>
2022-06-30 12:58:50 -0700

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    }
+    
+    let goldRatio = 1.618034;
+    return +((Math.pow(goldRatio, index) - Math.pow((1 - goldRatio), index)) / Math.sqrt(5)).toFixed(0);
 };
 
 // Do not edit below this line
