3b9354e988e0b11419dcce2990afc4d2736ca534
Alexander Winters <wintersalex@icloud.com>
2022-07-05 11:50:36 -0700

    Complete 13_caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -11,14 +11,10 @@ const caesar = function(text, shift) {
 function shiftCode(charCodeArr, shift) {
     let result = [];
     for (let i = 0; i < charCodeArr.length; i++) {
-        if (charCodeArr[i] != 33 && charCodeArr[i] != 44 && charCodeArr[i] != 32) {
-            if ((charCodeArr[i] + shift) > 90 && (charCodeArr[i] - shift) < 90) {
-                charCodeArr[i] = ((charCodeArr[i] + shift) - 90) + 64;  // Wraps to front of capital unicode values
-            } else if ((charCodeArr[i] + shift) < 65 && (charCodeArr[i] - shift) > 65) {
-                charCodeArr[i] = ((charCodeArr[i] + shift) - 64) + 90;  // Wraps to end of capital unicode values
-            } else {
-                charCodeArr[i] += shift;
-            }
+        if (isCapitalUnicode(charCodeArr[i])) {
+            charCodeArr[i] = shiftCapital(charCodeArr[i], shift)
+        } else if (isLowerUnicode(charCodeArr[i])) {
+            charCodeArr[i] = shiftLower(charCodeArr[i], shift)
         }
         result[i] = charCodeArr[i];
         result[i] = String.fromCharCode(result[i]);
@@ -41,13 +37,37 @@ function isLowerUnicode(value) {
     return false;
 }
 
-function shiftCapital() {
-
+function shiftCapital(value, shift) {
+    let temp = shift;
+    while (temp > 25) {
+        shift -= 26;    // Subtracts a full 'loop' of the available unicode characters
+        temp -= shift;
+    }
+    if ((value + shift) > 90 && (value - shift) < 90) {
+        value = ((value + shift) - 90) + 64;  // Wraps to front of capital unicode values
+    } else if ((value + shift) < 65 && (value - shift) > 65) {
+        value = ((value + shift) - 64) + 90;  // Wraps to end of capital unicode values
+    } else {
+        value += shift;
+    }
+    return value;
 }
 
 
-function shiftLower() {
-    
+function shiftLower(value, shift) {
+    let temp = shift;
+    while (temp > 25) {
+        shift -= 26;
+        temp -= shift;
+    }
+    if ((value + shift) > 122 && (value - shift) < 122) {
+        value = ((value + shift) - 122) + 96;
+    } else if ((value + shift) < 97 && (value - shift) > 97) {
+        value = ((value + shift) - 96) + 122;
+    } else {
+        value += shift;
+    }
+    return value;
 }
 // Do not edit below this line
 module.exports = caesar;
